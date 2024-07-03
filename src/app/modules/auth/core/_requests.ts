9e82877ab1f7ccd020586e896b6dc78bc520afb4
AbanobW <154471806+AbanobW@users.noreply.github.com>
2024-07-03 16:25:30 +0300

    Update _requests.ts

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -1,27 +1,27 @@
+import axios from "axios";
 import { AuthModel, UserModel } from "./_models";
+
+  
+
+// export const GET_USER_BY_ACCESSTOKEN_URL = `https://preview.keenthemes.com/metronic8/laravel/api/verify_token`;
+// export const LOGIN_URL = `https://preview.keenthemes.com/metronic8/laravel/api/login`;
+// export const REGISTER_URL = `https://preview.keenthemes.com/metronic8/laravel/api/register`;
+// export const REQUEST_PASSWORD_URL = `https://preview.keenthemes.com/metronic8/laravel/api/forgot_password`;
 
 export const GET_USER_BY_ACCESSTOKEN_URL = `http://167.172.165.109:8080/api/v1/auth/refresh`;
 export const LOGIN_URL = `http://167.172.165.109:8080/api/v1/auth/login`;
 export const REGISTER_URL = `http://167.172.165.109:8080/api/v1/auth/register`;
 export const REQUEST_PASSWORD_URL = `https://preview.keenthemes.com/metronic8/laravel/api/forgot_password`;
 
-// Login function using fetch
+// Server should return AuthModel
 export function login(email: string, password: string) {
-  return fetch(LOGIN_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json() as Promise<AuthModel>;
+  return axios.post<AuthModel>(LOGIN_URL, {
+    email,
+    password,
   });
 }
 
-// Register function using fetch
+// Server should return AuthModel
 export function register(
   email: string,
   firstname: string,
@@ -29,54 +29,24 @@ export function register(
   password: string,
   password_confirmation: string
 ) {
-  return fetch(REGISTER_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email,
-      first_name: firstname,
-      last_name: lastname,
-      password,
-      password_confirmation,
-    }),
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json() as Promise<AuthModel>;
+  return axios.post(REGISTER_URL, {
+    email,
+    first_name: firstname,
+    last_name: lastname,
+    password,
+    password_confirmation,
   });
 }
 
-// Request Password function using fetch
+// Server should return object => { result: boolean } (Is Email in DB)
 export function requestPassword(email: string) {
-  return fetch(REQUEST_PASSWORD_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email }),
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json() as Promise<{ result: boolean }>;
+  return axios.post<{ result: boolean }>(REQUEST_PASSWORD_URL, {
+    email,
   });
 }
 
-// Get User By Token function using fetch
 export function getUserByToken(token: string) {
-  return fetch(GET_USER_BY_ACCESSTOKEN_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ api_token: token }),
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json() as Promise<UserModel>;
+  return axios.post<UserModel>(GET_USER_BY_ACCESSTOKEN_URL, {
+    api_token: token,
   });
 }
