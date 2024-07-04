da0fb2e293990c2426579efee0f8b4e68c3589df
AbanobW <abanob.wagdi@gmail.com>
2024-07-04 19:01:06 +0300

    fetch login

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -19,6 +19,7 @@ export async function login(email: string, password: string): Promise<AuthModel>
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Accept': 'application/json',
     },
     body: JSON.stringify({
       email,
