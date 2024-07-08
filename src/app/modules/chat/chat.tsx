1d293bec8a69f934ea9a63d8538c6e22e6060c33
AbanobW <abanob.wagdi@gmail.com>
2024-07-08 21:09:43 +0300

    chaty

--- src/app/modules/chat/chat.tsx
+++ src/app/modules/chat/chat.tsx
@@ -1,15 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useIntl } from "react-intl";
-
-
-
+import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 
 const ChatWrapper = () => {
 	const intl = useIntl();
+
+	useEffect(() => {
+		window.echo_tenant = "miracle.echoglobal.org";
+		window.echo_uuid = "abebb8ca-bad4-4051-b890-bcc147b60ac5";
+	}, []);
 	return (
 		<>
-				<div className="js-widget echo-chat-widget">
-    </div>
+			<div className="js-widget echo-chat-widget"></div>
+
+			<h1>test</h1>
+
+			<Helmet>
+				<script
+					src="https://miracle.echoglobal.org/client/websites/full_frame_widget.js?v=6"
+					type="text/javascript"
+				></script>{" "}
+			</Helmet>
 		</>
 	);
 };
