ced6dbcd61891cb56b0ee2e7f3570edea4975c5a
AbanobW <abanob.wagdi@gmail.com>
2024-07-16 19:30:09 +0300

    subjects6

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/',
   build: {
     chunkSizeWarningLimit: 3000,
+    outDir: 'dist', // Ensure the output directory is correctly set
   },
-})
+  server: {
+    port: 3000, // Adjust the port if needed
+  },
+});
