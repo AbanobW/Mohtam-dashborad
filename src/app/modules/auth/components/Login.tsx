da42081098ab8c81b4e3540024371e16cecff688
AbanobW <abanob.wagdi@gmail.com>
2024-07-17 01:18:35 +0300

    login

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -1,19 +1,17 @@
-
-import {useState} from 'react'
+import { useState } from 'react'
 import * as Yup from 'yup'
 import clsx from 'clsx'
-import {Link} from 'react-router-dom'
-import {useFormik} from 'formik'
-import {getUserByToken, login} from '../core/_requests'
-import {toAbsoluteUrl} from '../../../../_metronic/helpers'
-import {useAuth} from '../core/Auth'
+import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { getUserByToken, login } from '../core/_requests'
+import { toAbsoluteUrl } from '../../../../_metronic/helpers'
+import { useAuth } from '../core/Auth'
 
 const loginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Wrong email format')
+  userName: Yup.string()
     .min(3, 'Minimum 3 symbols')
     .max(50, 'Maximum 50 symbols')
-    .required('Email is required'),
+    .required('User Name is required'),
   password: Yup.string()
     .min(3, 'Minimum 3 symbols')
     .max(50, 'Maximum 50 symbols')
@@ -21,32 +19,23 @@ const loginSchema = Yup.object().shape({
 })
 
 const initialValues = {
-  email: 'admon@gmail.com',
+  userName: 'admon',
   password: '1q2w3eX#',
 }
 
-/*
-  Formik+YUP+Typescript:
-  https://jaredpalmer.com/formik/docs/tutorial#getfieldprops
-  https://medium.com/@maurice.de.beijer/yup-validation-and-typescript-and-formik-6c342578a20e
-*/
-
 export function Login() {
   const [loading, setLoading] = useState(false)
-  const {saveAuth, setCurrentUser} = useAuth()
+  const { saveAuth, setCurrentUser } = useAuth()
 
   const formik = useFormik({
     initialValues,
     validationSchema: loginSchema,
-    onSubmit: async (values, {setStatus, setSubmitting}) => {
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
       setLoading(true)
       try {
-        const auth = await login(values.email, values.password)
-        // const {data: auth} = await login(values.email, values.password)
+        const auth = await login(values.userName, values.password)
         saveAuth(auth)
-        // const {data: user} = await getUserByToken(auth.api_token)
-        const {data: user} = await getUserByToken(auth.accessToken)
-
+        const { data: user } = await getUserByToken(auth.accessToken)
         setCurrentUser(user)
       } catch (error) {
         console.error(error)
@@ -65,16 +54,12 @@ export function Login() {
       noValidate
       id='kt_login_signin_form'
     >
-      {/* begin::Heading */}
       <div className='text-center mb-11'>
-      <Link to='/' className='mb-12'>
-            <img alt='Logo' src={toAbsoluteUrl('media/logos/demo6light.svg')} className='h-75px mb-5' />
-          </Link>
+        <Link to='/' className='mb-12'>
+          <img alt='Logo' src={toAbsoluteUrl('media/logos/demo6light.svg')} className='h-75px mb-5' />
+        </Link>
         <h1 className='text-gray-900 fw-bolder mb-3'>Sign In</h1>
-        
       </div>
-      {/* begin::Heading */}
-
 
       {formik.status ? (
         <div className='mb-lg-15 alert alert-danger'>
@@ -83,38 +68,32 @@ export function Login() {
       ) : (
         <div className='mb-10 bg-light-info p-8 rounded'>
           <div className='text-info'>
-            Use account <strong>admin@demo.com</strong> and password <strong>demo</strong> to
-            continue.
+            Use account <strong>admin</strong> and password <strong>demo</strong> to continue.
           </div>
         </div>
       )}
 
-      {/* begin::Form group */}
       <div className='fv-row mb-8'>
-        <label className='form-label fs-6 fw-bolder text-gray-900'>Email</label>
+        <label className='form-label fs-6 fw-bolder text-gray-900'>User Name</label>
         <input
-          placeholder='Email'
-          {...formik.getFieldProps('email')}
+          placeholder='User Name'
+          {...formik.getFieldProps('userName')}
           className={clsx(
             'form-control bg-transparent',
-            {'is-invalid': formik.touched.email && formik.errors.email},
-            {
-              'is-valid': formik.touched.email && !formik.errors.email,
-            }
+            { 'is-invalid': formik.touched.userName && formik.errors.userName },
+            { 'is-valid': formik.touched.userName && !formik.errors.userName }
           )}
-          type='email'
-          name='email'
+          type='text'
+          name='userName'
           autoComplete='off'
         />
-        {formik.touched.email && formik.errors.email && (
+        {formik.touched.userName && formik.errors.userName && (
           <div className='fv-plugins-message-container'>
-            <span role='alert'>{formik.errors.email}</span>
+            <span role='alert'>{formik.errors.userName}</span>
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Form group */}
       <div className='fv-row mb-3'>
         <label className='form-label fw-bolder text-gray-900 fs-6 mb-0'>Password</label>
         <input
@@ -123,12 +102,8 @@ export function Login() {
           {...formik.getFieldProps('password')}
           className={clsx(
             'form-control bg-transparent',
-            {
-              'is-invalid': formik.touched.password && formik.errors.password,
-            },
-            {
-              'is-valid': formik.touched.password && !formik.errors.password,
-            }
+            { 'is-invalid': formik.touched.password && formik.errors.password },
+            { 'is-valid': formik.touched.password && !formik.errors.password }
           )}
         />
         {formik.touched.password && formik.errors.password && (
@@ -139,21 +114,11 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Wrapper */}
       <div className='d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8'>
         <div />
-
-        {/* begin::Link */}
-        {/* <Link to='/auth/forgot-password' className='link-primary'>
-          Forgot Password ?
-        </Link> */}
-        {/* end::Link */}
       </div>
-      {/* end::Wrapper */}
 
-      {/* begin::Action */}
       <div className='d-grid mb-10'>
         <button
           type='submit'
@@ -163,21 +128,13 @@ export function Login() {
         >
           {!loading && <span className='indicator-label'>Continue</span>}
           {loading && (
-            <span className='indicator-progress' style={{display: 'block'}}>
+            <span className='indicator-progress' style={{ display: 'block' }}>
               Please wait...
               <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
             </span>
           )}
         </button>
       </div>
-      {/* end::Action */}
-{/* 
-      <div className='text-gray-500 text-center fw-semibold fs-6'>
-        Not a Member yet?{' '}
-        <Link to='/auth/registration' className='link-primary'>
-          Sign up
-        </Link>
-      </div> */}
     </form>
   )
 }
--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -1,28 +1,12 @@
 import axios from "axios";
 import { AuthModel, UserModel } from "./_models";
 
-  
-
-// export const GET_USER_BY_ACCESSTOKEN_URL = `https://preview.keenthemes.com/metronic8/laravel/api/verify_token`;
-// export const LOGIN_URL = `https://preview.keenthemes.com/metronic8/laravel/api/login`;
-// export const REGISTER_URL = `https://preview.keenthemes.com/metronic8/laravel/api/register`;
-// export const REQUEST_PASSWORD_URL = `https://preview.keenthemes.com/metronic8/laravel/api/forgot_password`;
-
 export const GET_USER_BY_ACCESSTOKEN_URL = `https://api.mohtam.org/api/v1/auth/refresh`;
 export const LOGIN_URL = `https://api.mohtam.org/api/v1/auth/login`;
 export const REGISTER_URL = `https://api.mohtam.org/api/v1/auth/register`;
 export const REQUEST_PASSWORD_URL = `https://preview.keenthemes.com/metronic8/laravel/api/forgot_password`;
 
-// Server should return AuthModel
-// export function login(email: string, password: string) {
-//   return axios.post<AuthModel>(LOGIN_URL, {
-//     email,
-//     password,
-//   });
-// }
-
-
-export async function login(email: string, password: string): Promise<AuthModel> {
+export async function login(userName: string, password: string): Promise<AuthModel> {
   const response = await fetch(LOGIN_URL, {
     method: 'POST',
     headers: {
@@ -30,7 +14,7 @@ export async function login(email: string, password: string): Promise<AuthModel>
       'Accept': 'application/json',
     },
     body: JSON.stringify({
-      email,
+      userName,
       password,
     }),
   });
@@ -44,7 +28,6 @@ export async function login(email: string, password: string): Promise<AuthModel>
   return data;
 }
 
-// Server should return AuthModel
 export function register(
   email: string,
   firstname: string,
@@ -61,10 +44,9 @@ export function register(
   });
 }
 
-// Server should return object => { result: boolean } (Is Email in DB)
-export function requestPassword(email: string) {
+export function requestPassword(userName: string) {
   return axios.post<{ result: boolean }>(REQUEST_PASSWORD_URL, {
-    email,
+    userName,
   });
 }
 
